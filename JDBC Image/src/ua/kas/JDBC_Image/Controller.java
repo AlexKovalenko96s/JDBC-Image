60a660028aa90ec060ba06c494c408a42dedc871
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-03-30 17:06:08 +0300

    add image in mySQL (blob), and extract image mySQL (file, not on ImageView)

---
+++ JDBC Image/src/ua/kas/JDBC_Image/Controller.java
@@ -0,0 +1,106 @@
+package ua.kas.JDBC_Image;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+	@FXML
+	TextField textID;
+	@FXML
+	TextField textNAME;
+	@FXML
+	TextArea area;
+	@FXML
+	ImageView imv;
+	String s;
+
+	public void browse(ActionEvent e) {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("*.IMAGE", "jpg", "gif", "png");
+		fileChooser.addChoosableFileFilter(filter);
+		int result = fileChooser.showSaveDialog(null);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fileChooser.getSelectedFile();
+			String path = selectedFile.getAbsolutePath();
+			System.out.println(path);
+			String ss = path.substring(path.indexOf("&"));
+			s = path;
+			imv.setImage(new javafx.scene.image.Image(getClass().getResourceAsStream("/res/" + ss)));
+
+		} else if (result == JFileChooser.CANCEL_OPTION) {
+			System.out.println("No Data");
+		}
+	}
+
+	public void add(ActionEvent e) throws SQLException, FileNotFoundException {
+		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/freemove", "root", "root");
+		java.sql.PreparedStatement myStmt = myConn
+				.prepareStatement("insert into cats(id,name,area,pic) values (?,?,?,?)");
+		InputStream is = new FileInputStream(new File(s));
+		myStmt.setString(1, textID.getText());
+		myStmt.setString(2, textNAME.getText());
+		myStmt.setString(3, area.getText());
+		myStmt.setBlob(4, is);
+		myStmt.executeUpdate();
+		System.out.println("Complet!");
+	}
+
+	public void see(ActionEvent e) throws SQLException, IOException {
+
+		Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/freemove", "root", "root");
+		String n = textNAME.getText();
+		java.sql.PreparedStatement ps = myConn.prepareStatement("select * from cats where name=?");
+		ps.setString(1, n);
+		ResultSet myRs = ps.executeQuery();
+
+		Blob img;
+		byte[] imgData = null;
+		
+		while (myRs.next()) {
+			textID.setText(myRs.getString("id"));
+			textNAME.setText(myRs.getString("name"));
+			area.setText(myRs.getString("area"));
+			img = myRs.getBlob("pic");
+			imgData = img.getBytes(1, (int) img.length());
+			
+			/*
+			 * извлекалка на рабочий стол
+			 */	
+			String dirName="C:\\Users\\KLUBnyaKprO\\Desktop";
+			BufferedImage imag=ImageIO.read(new ByteArrayInputStream(imgData));
+			//Image i = (Image) ImageIO.getImageReaders(imag);
+			//imv.setImage(i);
+			ImageIO.write(imag, "jpg", new File(dirName,"&"+myRs.getString("name")+".jpg"));	
+			/* 
+			 */
+			
+			
+		}
+		System.out.println("complet");
+	}
+}
---
+++ JDBC Image/src/ua/kas/JDBC_Image/Main.java
@@ -0,0 +1,27 @@
+package ua.kas.JDBC_Image;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	Parent root;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

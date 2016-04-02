dee95f077cf07ac22c1a5643a9c6211fb4844b2e
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-04-02 17:06:30 +0300

    cont///

--- JDBC Image/src/ua/kas/JDBC_Image/Controler_pic.java
+++ JDBC Image/src/ua/kas/JDBC_Image/Controler_pic.java
@@ -3,6 +3,7 @@ package ua.kas.JDBC_Image;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
@@ -15,9 +16,13 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class Controler_pic implements Initializable{
 
@@ -306,5 +311,13 @@ public class Controler_pic implements Initializable{
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void back(ActionEvent e) throws IOException{
+		Scene main_scene = new Scene(FXMLLoader.load(getClass().getResource("Main.fxml")));
+		main_scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage app_stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		app_stage.setScene(main_scene);
+		app_stage.show();
 	}
 }
